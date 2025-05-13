4f1dfb0c478416bb14eaa9a3c1fb27ee714fbba2
Pchass <pchass2703@gmail.com>
2025-05-12 23:05:01 -0400

    Obtener ruta

--- src/api.py
+++ src/api.py
@@ -5,3 +5,13 @@ import json
 app = Flask(__name__)
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), '../../data/extracted_info.json')
+
+@app.route('/api/empresas', methods=['GET'])
+def get_all_data():
+    if not os.path.exists(DATA_PATH):
+        return jsonify({"error": "Archivo de datos no encontrado."}), 404
+
+    with open(DATA_PATH, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    return jsonify(data)
