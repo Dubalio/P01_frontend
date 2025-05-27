458c6d05095e6135c1b744292ad9a84dcb8d812a
Pchass <pchass2703@gmail.com>
2025-05-27 00:37:23 -0400

    Login buttons

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,7 +13,7 @@ function Login({ onLogin, switchToRegister }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
-    
+
     try {
       setLoading(true);
       const response = await loginUser({ email, password, role });
@@ -66,7 +66,7 @@ function Login({ onLogin, switchToRegister }) {
         </button>
       </form>
       <p className="switch-auth">
-        ¿Ya tienes una cuenta? <Link to="/login">Iniciar Sesión</Link>
+        ¿No tienes una cuenta? <Link to="/Register">Registrar Sesión</Link>
       </p>
     </div>
   );
